ca76049b5ce15e64f417e3f468fd571e37f06feb
Diptiy23 <68714993+Diptiy23@users.noreply.github.com>
2020-07-26 22:39:22 +0100

    Routes Folder
    web.php

---
+++ web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/video_list', function () {
+    return view('video_list');
+});
+
+Route::post('video_list/submit','video_listController@search');
